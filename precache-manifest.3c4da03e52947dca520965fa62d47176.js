789b67946740b275fc979394138cf1e10a212216
Nguyen Duc Dai Nghia <nghia.nguyenduc@axonactive.com>
2019-11-11 15:48:09 +0700

    Init Project

---
+++ precache-manifest.3c4da03e52947dca520965fa62d47176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6babd1aebb62c16c60dd25fc62810b05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5470e2223e1f20b6d30d",
+    "url": "/static/css/main.60f780ec.chunk.css"
+  },
+  {
+    "revision": "30905732330ed52658ff",
+    "url": "/static/js/2.4cf74a33.chunk.js"
+  },
+  {
+    "revision": "5470e2223e1f20b6d30d",
+    "url": "/static/js/main.c187188f.chunk.js"
+  },
+  {
+    "revision": "b4119faac75ac415a4b1",
+    "url": "/static/js/runtime-main.5e4d536e.js"
+  }
+]);
